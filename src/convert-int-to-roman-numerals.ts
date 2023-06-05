dc01ca2cfce74af913655ed35d2e8d5a1648eb01
MilesBarriball-Saw <miles.barriball-saw@lasalle-education.com>
2023-06-05 22:12:40 +0100

    Add function to convert integer to roman numerals

---
+++ src/convert-int-to-roman-numerals.ts
@@ -0,0 +1,32 @@
+function convertIntToRomanNumeral(num: number): string {
+  if (num <= 0) {
+    throw new Error('Input must be a positive integer');
+  }
+
+  const romanNumerals: [number, string][] = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ];
+
+  let result = '';
+
+  for (const [value, numeral] of romanNumerals) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
+    }
+  }
+
+  return result;
+}
